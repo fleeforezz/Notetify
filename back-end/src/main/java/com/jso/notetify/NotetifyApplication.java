bae016d4212aca8c5ac25777aed5b48488b216f6
Trương Minh Nhật <fleeforezz@gmail.com>
2026-03-09 12:31:13 +0700

    Init back-end code

---
+++ back-end/src/main/java/com/jso/notetify/NotetifyApplication.java
@@ -0,0 +1,13 @@
+package com.jso.notetify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotetifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotetifyApplication.class, args);
+	}
+
+}
